ddeb33895f8cf836bfb93db5e674798ba9677f42
mhji <battlesun99@gmail.com>
2021-08-23 22:18:08 +0900

    Feat: make corona stats site app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from './components/Header';
+import Contents from './components/Contents';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Contents />
+    </div>
+  );
+}
+
+export default App;
