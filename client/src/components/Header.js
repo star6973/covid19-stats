6c096a0c3c24ac053324c8d91e6a6f46d521edbe
Ji Myung Hwa <battlesun99@gmai.com>
2021-08-27 01:56:41 +0900

    Refactor: move client folder

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>COVID-19</h1>
+            <select>
+                <option>국내</option>
+                <option>세계</option>
+            </select>
+        </header>
+    )
+}
+
+export default Header;
