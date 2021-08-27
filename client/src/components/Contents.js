6dfa9b64808825cd1059642a679064c329777c2d
mhji <battlesun99@gmail.com>
2021-08-27 17:56:07 +0900

    Feat: draw ResponsiveLine hooks

--- client/src/components/Contents.js
+++ client/src/components/Contents.js
@@ -1,11 +1,38 @@
 import { useState, useEffect } from "react";
-import { Bar, Doughnut, Line } from "react-chartjs-2";
-import Axios from "axios";
+import { ResponsiveLine  } from "@nivo/line";
+import { ResponsivePie } from "@nivo/pie";
+import axios from "axios";
 
-const confirmedOptions = {
-    title: { display: true, text: "누적 확진자 추이", fontSize: 16 },
-    legend: { display: true, position: "bottom" },
+const confirmedTheme = {
+    fontFamily: "BMHANNAPro",
+    fontSize: 6,
+    textColor: "salmon",
+    labelTextColor: "salmon",
+    curve: "linear",
+    scheme: "nivo",
 };
+
+const confirmedOption = {
+    margin: { top: 50, right: 80, bottom: 50, left: 60 },
+    xScale: { type: 'point' },
+    yScale: { type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false },
+    yFormat: " >-.2f",
+    axisBottom: {
+        legend: 'Confirmed',
+        legendOffset: 36,
+        legendPosition: 'middle'
+    },
+    axisLeft: {
+        legend: 'Count',
+        legendOffset: -45,
+        legendPosition: 'middle'
+    },
+    pointSize: 8,
+    pointBorderWidth: 2,
+    pointBorderColor: { from: 'serieColor' },
+    pointLabelYOffset: -12,
+    useMesh: true,
+}
 
 const quarantinedOptions = {
     title: { display: true, text: "월별 격리자 현황", fontSize: 16 },
@@ -19,102 +46,88 @@ const comparedOptions = {
 
 const Contents = () => {
     const [confirmedData, setConfirmedData] = useState({});
-    const [quarantinedData, setQuarantinedData] = useState({});
-    const [comparedData, setComparedData] = useState({});
 
     useEffect(() => {
         const fetchEvents = async () => {
-            const res = await Axios.get("https://api.covid19api.com/total/dayone/country/kr");
-            const response = await Axios.get("/api/covid");
-            console.log(response);
-            console.log(response.data.items);
+            const response = await axios.get("/api/covid");
 
-            makeData(res.data);
+            //* 1. 확진자 추이 그래프
+            //* 2. 확진자 수, 격리해제 수, 검사진행 수, 사망자 수, 치료진행 수 정보표
+            //* 3. 
+
+            //~ decideCnt: 확진자 수
+            //~ clearCnt: 격리해제 수
+            //~ examCnt: 검사진행 수
+            //~ deathCnt: 사망자 수
+            //~ careCnt: 치료 중 환자 수
+            
+            const response_data = (response.data.items).item.sort((a, b) => a.stateDt - b.stateDt);
+            console.log(response_data);
+            createDataForm(response_data);
         };
-        const makeData = (items) => {
-            const dataList = items.reduce((acc, cur) => {
-                const nowDate = new Date(cur.Date);
-                const year = nowDate.getFullYear();
-                const month = nowDate.getMonth() + 1;
-                const date = nowDate.getDate();
-                const confirmed = cur.Confirmed;
-                const active = cur.Active;
-                const deaths = cur.Deaths;
-                const recovered = cur.Recovered;
 
-                const findItem = acc.find(el => el.year === year && el.month === month);
+        const createDataForm = items => {
+            const dataList = items.reduce((acc, cur) => {
+                const createDate = new Date(cur.createDt);
+                const formDate = `${createDate.getFullYear()}-${(createDate.getMonth() + 1).toString().padStart(2, "0")}`;
+                const decideCount = cur.decideCnt;
+                const clearCount = cur.clearCnt;
+                const examCount = cur.examCnt;
+                const deathCount = cur.deathCnt;
+                const careCount = cur.careCnt;
+                const findItem = acc.find(el => el.formDate === formDate);
+            
                 if (!findItem) {
-                    acc.push({ year, month, date, confirmed, active, deaths, recovered });
+                    acc.push({ formDate, decideCount, clearCount, examCount, deathCount, careCount });
                 }
-                if (findItem && findItem.date < date) {
-                    findItem.year = year;
-                    findItem.month = month;
-                    findItem.date = date;
-                    findItem.confirmed = confirmed;
-                    findItem.active = active;
-                    findItem.deaths = deaths;
-                    findItem.recovered = recovered;
+            
+                if (findItem) {
+                    findItem.formDate = formDate;
+                    findItem.decideCount = decideCount;
+                    findItem.clearCount = clearCount;
+                    findItem.examCount = examCount;
+                    findItem.deathCount = deathCount;
+                    findItem.careCount = careCount;
                 }
+            
                 return acc;
             }, []);
 
-            setConfirmedData({                
-                labels: dataList.map(el => `${el.month}월`),
-                datasets: [
-                    {
-                        label: "국내 누적 확진자",
-                        backgroundColor: "salmon",
-                        fill: true,
-                        data: dataList.map(el => el.confirmed)
-                    }
-                ]
-            })
+            setConfirmedData([{
+                "id": "Count",
+                "data":
+                    dataList.map((val, idx) => {
+                        return {
+                            "x": val.formDate,
+                            "y": val.decideCount,
+                        }
+                    })
+            }])
 
-            setQuarantinedData({
-                labels: dataList.map(el => `${el.month}월`),
-                datasets: [
-                    {
-                        label: "월별 격리자 현황",
-                        borderColor: "salmon",
-                        fill: false,
-                        data: dataList.map(el => el.active)
-                    }
-                ]
-            })
-
-            const lastData = dataList[dataList.length - 1];
-            setComparedData({
-                labels: ["확진", "해제", "사망"],
-                datasets: [
-                    {
-                        label: "누적 확진, 해제, 사망",
-                        backgroundColor: ["#ff3d67", "#058bff", "#ffc233"],
-                        borderColor: ["#ff3d67", "#058bff", "#ffc233"],
-                        fill: false,
-                        data: [lastData.confirmed, lastData.recovered, lastData.deaths]
-                    }
-                ]
-            })
         }
-
         fetchEvents();
-    }, [])
+
+    }, [setConfirmedData])
 
     return (
-        <section>
+        <div className="App-contents">
             <h2>국내 코로나 현황</h2>
-            <div className="App-contents">
-                <div>
-                    <Bar data={confirmedData} options={confirmedOptions} />
-                </div>
-                <div>
-                    <Line data={quarantinedData} options={quarantinedOptions} />
-                </div>
-                <div>
-                    <Doughnut data={comparedData} options={comparedOptions} />
-                </div>
+            <div className="App-chart-confirmed">
+                {
+                    Object.keys(confirmedData).length !== 0 ?
+                        <div>
+                            <ResponsiveLine
+                                data={confirmedData}
+                                theme={confirmedTheme}
+                                {...confirmedOption}
+                            />
+                            <ResponsivePie />
+                        </div>
+                    :
+                        <div>Loading...</div>
+                }
             </div>
-        </section>
+        </div>
     )
 }
 
